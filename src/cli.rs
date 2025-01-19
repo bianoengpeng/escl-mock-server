4a3cdfc37c00a90df52afeacde91fa4870177514
Christian Nagel <chris.imx@online.de>
2025-01-19 13:49:13 +0100

    Add GPLv3 license

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,26 @@
-use std::fmt::{Display, Formatter};
+/*
+ *     Copyright (C) 2024-2025 Christian Nagel and contributors
+ *
+ *     This file is part of escl-mock-server.
+ *
+ *     escl-mock-server is free software: you can redistribute it and/or modify it under the terms of
+ *     the GNU General Public License as published by the Free Software Foundation, either
+ *     version 3 of the License, or (at your option) any later version.
+ *
+ *     escl-mock-server is distributed in the hope that it will be useful, but WITHOUT ANY
+ *     WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *     FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along with eSCLKt.
+ *     If not, see <https://www.gnu.org/licenses/>.
+ *
+ *     SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 use clap::error::ErrorKind;
 use clap::{arg, CommandFactory, Parser};
 use regex::Regex;
+use std::fmt::{Display, Formatter};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
