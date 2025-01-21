682c93fc2a278b66ac714977c50aac8cc7785259
Christian Nagel <chris.imx@online.de>
2025-01-21 21:11:49 +0100

    Add android env option to nix flake

--- flake.nix
+++ flake.nix
@@ -9,19 +9,59 @@
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
+        (system: f { pkgs = import nixpkgs { inherit system; config = { 
+          android_sdk.accept_license = true;
+          allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+             "android-sdk-cmdline-tools"
+             "android-sdk-tools"
+           ];
+        }; }; });
       overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+      flake-conf = (builtins.fromTOML (builtins.readFile ./flake-conf.toml));
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
 
-        default = pkgs.mkShell {
+        default = 
+    let
+    android-env = pkgs.androidenv.composeAndroidPackages {
+          #cmdLineToolsVersion = "8.0";
+          #toolsVersion = "26.1.1";
+          #platformToolsVersion = "30.0.5";
+          #buildToolsVersions = [ "30.0.3" ];
+          includeEmulator = false;
+          #emulatorVersion = "30.3.4";
+          #platformVersions = [ "28" "29" "30" ];
+          includeSources = false;
+          includeSystemImages = false;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          #cmakeVersions = [ "3.10.2" ];
+          includeNDK = true;
+          #ndkVersions = ["22.0.7026061"];
+          useGoogleAPIs = false;
+          useGoogleTVAddOns = false;
+          includeExtras = [
+            "extras;google;gcm"
+          ];
+        };
+    in 
+        pkgs.mkShell rec {
+
           buildInputs = with pkgs; [
             clang
             # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
             llvmPackages.bintools
             rustup
-          ];
+            cargo-ndk
+            android-env.androidsdk
+          ] ++ lib.optional flake-conf.android androidenv.androidPkgs.ndk-bundle;
           RUSTC_VERSION = overrides.toolchain.channel;
+
+          inherit (pkgs.lib.optionalAttrs flake-conf.android {
+            ANDROID_SDK_ROOT = "${android-env.androidsdk}/libexec/android-sdk";
+            ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+          }) ANDROID_NDK_ROOT ANDROID_SDK_ROOT;
+          #GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
           # https://github.com/rust-lang/rust-bindgen#environment-variables
           LIBCLANG_PATH =
             pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
